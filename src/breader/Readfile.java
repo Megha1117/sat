d3c8f8f2e62f791c24338c81ea5069b335dd9aea
megha1117 <meghamapalagama@yahoo.com>
2018-12-08 15:16:58 -0500

    project11

---
+++ src/breader/Readfile.java
@@ -0,0 +1,18 @@
+package breader;
+
+public class Readfile {
+	
+	@Suppresswarnings("resource")
+	public static void main(string[] args){
+	   String path = "/User/peoplentech/develop/seleniumNovember2018/filet.txt";
+	   FileReader fr + null;
+	   Buffereader br null;
+	   
+	   try {
+		   fr = new Filereader(path);
+	   } catch FileReader(path);
+	   }
+		
+	}
+
+}
